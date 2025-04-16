4e4a94d2ce947a20b6eae3a33e66372df09a4a47
Tshebeletso <t.tjabane@gmail.com>
2025-04-16 14:11:07 +0200

    Get Menu Query and Handler

---
+++ Cantina.Core/UseCase/Handlers/GetMenuQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Cantina.Core.Dto;
+using Cantina.Core.Interface;
+using Cantina.Core.UseCase.Requests;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina.Core.UseCase.Handlers
+{
+    public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, List<MenuItem>>
+    {
+        private readonly IMenuItemRepository _menuItemRepository;
+        public GetMenuQueryHandler(IMenuItemRepository menuItemRepository)
+        {
+            _menuItemRepository = menuItemRepository;
+        }
+        public async Task<List<MenuItem>> Handle(GetMenuQuery request, CancellationToken cancellationToken)
+        {
+            return await _menuItemRepository.GetAllMenuItemsAsync();
+        }
+    }
+}
---
+++ Cantina.Core/UseCase/Requests/GetMenuQuery.cs
@@ -0,0 +1,17 @@
+﻿using Cantina.Core.Dto;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina.Core.UseCase.Requests
+{
+    public class GetMenuQuery: IRequest<List<MenuItem>>
+    {
+        public GetMenuQuery()
+        {
+        }
+    }
+}
