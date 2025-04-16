b50276367670fe6612078f0bc1e06b36e72d2cdc
Tshebeletso <t.tjabane@gmail.com>
2025-04-16 14:11:52 +0200

    initial Core

---
+++ Cantina.Core/Dto/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina.Core.Dto
+{
+    public class MenuItem
+    {
+        // Dishes & drinks must have a name, description, price, and image.
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public bool IsDrink { get; set; }
+    }
+}
---
+++ Cantina.Core/Interface/IMenuItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using Cantina.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina.Core.Interface
+{
+    public interface IMenuItemRepository
+    {
+        Task<List<MenuItem>> GetAllMenuItemsAsync();
+        Task AddMenuItemAsync(MenuItem menuItem);
+        Task UpdateMenuItemAsync(MenuItem menuItem);
+        Task DeleteMenuItemAsync(int id);
+    }
+}
