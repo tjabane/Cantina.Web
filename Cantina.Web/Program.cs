070712a51c2ef53f9f523be545e1de68cf388255
Tshebeletso <t.tjabane@gmail.com>
2025-04-16 19:08:04 +0200

    Open telemetry config

--- Cantina.Web/Program.cs
+++ Cantina.Web/Program.cs
@@ -1,10 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
+var builder = WebApplication.CreateBuilder(args);
+var applicationName = builder.Configuration["ApplicationName"] ?? "The Cantina";
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// MediatR
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Cantina.Core.UseCase.Handlers.GetMenuQueryHandler).Assembly));
+// Open Telemetry
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(applicationName))
+        .AddConsoleExporter();
+});
+
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(applicationName))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddConsoleExporter())
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddConsoleExporter());
 
 var app = builder.Build();
 
