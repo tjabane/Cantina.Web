43282a701a4d146144abbb784488a696cbbc52fc
Tshebeletso <t.tjabane@gmail.com>
2025-04-19 20:10:04 +0200

    User repository

---
+++ Cantina.Core/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Cantina.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina.Core.Interface
+{
+    public interface IUserRepository
+    {
+        Task<User> GetById(int Id);
+    }
+}
---
+++ Cantina.Infrastructure/Redis/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Cantina.Core.Dto;
+using Cantina.Core.Interface;
+using NRedisStack;
+using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantina.Infrastructure.Redis
+{
+    public class UserRepository(IConnectionMultiplexer redis) : IUserRepository
+    {
+        private readonly JsonCommands _jsonCMD = redis.GetDatabase().JSON();
+
+        public async Task<User> GetById(int id)
+        {
+            return await _jsonCMD.GetAsync<User>($"user:{id}");
+        }
+    }
+}
