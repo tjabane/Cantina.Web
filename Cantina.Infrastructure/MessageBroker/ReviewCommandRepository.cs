4070fcbce32cbc0d2633b5e6574b90bbe4300a9a
Tshebeletso <t.tjabane@gmail.com>
2025-04-19 19:30:01 +0200

    Review Writer and QUery

---
+++ Cantina.Infrastructure/MessageBroker/ReviewCommandRepository.cs
@@ -0,0 +1,30 @@
+﻿using Cantina.Core.Data.Entities;
+using Cantina.Core.Dto;
+using Cantina.Core.Interface;
+using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Cantina.Infrastructure.MessageBroker
+{
+    public class ReviewCommandRepository : IReviewCommandRepository
+    {
+        private readonly string _topic;
+        private readonly IProducer<Null, string> _producer;
+        public ReviewCommandRepository(string host, string topic)
+        {
+            _topic = topic;
+            var producerConfig = new ProducerConfig { BootstrapServers = host };
+            _producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+        }
+        public async Task AddAsync(ReviewDto review)
+        {
+            var message = JsonSerializer.Serialize(review);
+            await _producer.ProduceAsync(_topic, new Message<Null, string> { Value = message });
+        }
+    }
+}
---
+++ Cantina.Infrastructure/Redis/ReviewQueryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Cantina.Core.Dto;
+using Cantina.Core.Interface;
+using NRedisStack;
+using NRedisStack.RedisStackCommands;
+using NRedisStack.Search;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Cantina.Infrastructure.Redis
+{
+    public class ReviewQueryRepository(IConnectionMultiplexer redis) : IReviewQueryRepository
+    {
+        private readonly string _indexName = "reviews-index";
+        private readonly SearchCommands _searchCommands = redis.GetDatabase().FT();
+        public async Task<List<ReviewViewDto>> GetAllAsync()
+        {
+            var menuResponse = await _searchCommands.SearchAsync(_indexName, new Query("*"));
+            return [.. menuResponse.ToJson().Select(json => JsonSerializer.Deserialize<ReviewViewDto>(json))];
+        }
+    }
+}
