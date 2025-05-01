b8ca1ecb526f718e378ffac05077db02a7a6191c
Tshebeletso <t.tjabane@gmail.com>
2025-05-01 13:00:51 +0200

    Ai

--- Cantina.AI/main.py
+++ Cantina.AI/main.py
@@ -4,12 +4,17 @@ from models.sentiment import analyze_sentiment
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"status": "alive"}
 
+@app.get("/api/health")
+async def health():
+    return {"status": "healthy"}
 
-@app.post("/sentiment/")
-async def sentiment_analysis(data: list[Review  | None]):
+@app.post("/api/sentiment/")
+async def sentiment_analysis(data: list[Review]):
+    sentiment_results = analyze_sentiment(data)
+    return sentiment_results
+
+@app.post("/api/summary/")
+async def create_summary(data: list[Review]):
     sentiment_results = analyze_sentiment(data)
     return sentiment_results
